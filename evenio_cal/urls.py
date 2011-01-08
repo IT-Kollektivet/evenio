665982e244996205f3326095ffb70a54ced302a6
Simon Shine <shreddedglory@gmail.com>
2011-01-08 16:11:31 +0100

    Typos in views and urlconf

--- evenio_cal/urls.py
+++ evenio_cal/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('evenio_cal.views',
-    (r'^$', 'list'),
-    (r'^show/(?P<event_id>\d+)/$', 'show'),
-    (r'^create/(?P<event_id>\d+)/$', 'create'),
-    (r'^update/(?P<event_id>\d+)/$', 'update'),
+    (r'^$', 'list_events'),
+    (r'^show/(?P<event_id>\d+)/$', 'show_event'),
+    (r'^create/(?P<event_id>\d+)/$', 'create_event'),
+    (r'^update/(?P<event_id>\d+)/$', 'update_event'),
 )
