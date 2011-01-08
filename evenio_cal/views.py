0cf255f5ae0b21390c45ddb1f3c6e1c6e2d4afa9
Vidir Valberg Gudmundsson <vidir.valberg@gmail.com>
2011-01-08 16:22:48 +0100

    Trying to figure out those damned templates

--- evenio_cal/views.py
+++ evenio_cal/views.py
@@ -14,7 +14,7 @@ def show_event(request, event_id):
 
     event = Event.objects.all()
 
-    return object_detail(request, event, object_id=event_id)
+    return object_detail(request, event, object_id=event_id, template_name="templates/evenio/event_detail.html")
 
 
 def list_events(request):
@@ -24,7 +24,7 @@ def list_events(request):
 
     events = Event.objects.all()
 
-    return object_list(request, events)
+    return object_list(request, events, template_name="templates/event_list.html")
 
 
 def create_event(request):
@@ -32,7 +32,7 @@ def create_event(request):
         template: evenio/templates/event_form.html
     """
 
-    return create_object(request, Event)
+    return create_object(request, Event, template_name="templates/event_form.html")
 
 
 def update_event(request, event_id):
@@ -40,4 +40,4 @@ def update_event(request, event_id):
         template: evenio/templates/event_form.html
     """
 
-    return update_object(request, Event, event_id)
+    return update_object(request, Event, event_id, template_name="template/event_form.html")
