150224759d4829242c63ba8906cac3d45c06ca41
Simon Shine <shreddedglory@gmail.com>
2011-03-04 22:52:13 +0100

    URL'er, ikke helt færdige

--- evenio/urls.py
+++ evenio/urls.py
@@ -3,6 +3,9 @@ from django.http import Http404, HttpResponse
 
 urlpatterns = patterns('',
     (r'^$', (lambda x: HttpResponse("Goodbye world!", status=404))),
-    url(r'^list/$', 'evenio.views.list_events', name='list'),
-    url(r'^event/(\d+)$', 'evenio.views.show_event', name='show'),
+    url(r'^search$', 'evenio.views.search', name='search'),
+    url(r'^search/([\w-]+)$', 'evenio.views.search_events', name='search'),
+    url(r'^events/([\w-]+)$', 'evenio.views.show_event', name='show'),
+    url(r'^events/([\w-]+)/update$', 'evenio.views.update_event', name='update'),
+    url(r'^create$', 'evenio.views.create_event', name='create'),
 )
