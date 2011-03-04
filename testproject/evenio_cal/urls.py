8708f38dc4d563dd20ae73eda19b655baa22cb9d
Vidir Valberg Gudmundsson <vidir.valberg@gmail.com>
2011-03-04 21:19:27 +0100

    old evenio is no more\!

--- testproject/evenio_cal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('evenio_cal.views',
-    (r'^$', 'list_events'),
-    (r'^show/(?P<event_id>\d+)/$', 'show_event'),
-    (r'^create/$', 'create_event'),
-    (r'^update/(?P<event_id>\d+)/$', 'update_event'),
-)
