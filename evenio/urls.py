019f093ed61993358b8f54afc04438e0c5023c39
Simon Shine <shreddedglory@gmail.com>
2011-03-04 23:32:59 +0100

    URLs now match Jon's REST API

--- evenio/urls.py
+++ evenio/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls.defaults import *
 from django.http import Http404, HttpResponse
 
 urlpatterns = patterns('',
-    (r'^$', (lambda x: HttpResponse("Goodbye world!", status=404))),
+    url(r'^$', 'evenio.views.list_events', name='list'),
     url(r'^search$', 'evenio.views.search', name='search'),
     url(r'^search/([\w-]+)$', 'evenio.views.search_events', name='search'),
     url(r'^events/([\w-]+)$', 'evenio.views.show_event', name='show'),
     url(r'^events/([\w-]+)/update$', 'evenio.views.update_event', name='update'),
     url(r'^create$', 'evenio.views.create_event', name='create'),
+    # Lists for years, months and days are handled in a view.
+    # Put this view lastly because it'll probably catch some unwanted URLs.
+    url(r'^(\w+|\d+)(?:/(\w+|\d+))?$', 'evenio.views.list_events', name='list'),
 )
