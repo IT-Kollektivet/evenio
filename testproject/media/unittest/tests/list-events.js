5acd4bee777d561b40fca0b245426be514f98187
dotnetCarpenter <jon.ronnenberg@gmail.com>
2011-03-05 01:17:08 +0100

    more test

--- testproject/media/unittest/tests/list-events.js
+++ testproject/media/unittest/tests/list-events.js
@@ -2,43 +2,45 @@
  * tutorial for async testing: http://net.tutsplus.com/tutorials/javascript-ajax/how-to-test-your-javascript-code-with-qunit/
  */
 function successCallback(data, textStatus, jqXHR) {
-ok(true);
+  var status = data.status;
+  ok(true, status);
 }
 function failedCallback(data, textStatus, jqXHR) {
-ok(true, textStatus);
+  var status = data.status;
+  ok(true, status);
 }
 
 module("List Events", {
-setup: function () {
-//    this.uriBase = "http://192.168.1.102:8000/evenio/list";
-$.ajaxSetup({
-async: true,
+  setup: function () {
+    this.uriBase = "http://192.168.1.102:8000/evenio/";
+    $.ajaxSetup({
+      async: true
+    });
+  },
+  teardown: function () {
 
-});
-},
-teardown: function () {
-
-}
+  }
 });
 /* Test#1 */
 test('Testing event lists', 3, function () {
   stop();
+
   $.ajax(this.uriBase, {
     success: successCallback,
     error: failedCallback
   });
   
-  $.ajax(this.uriBase + '/2011/05', {
+  $.ajax(this.uriBase + '2011/05/', {
     success: successCallback,
     error: failedCallback
   });
 
-  $.ajax(this.uriBase + '/2011/05/01', {
+  $.ajax(this.uriBase + '2011/05/01', {
     success: successCallback,
     error: failedCallback
   });
   
   setTimeout(function() {  
     start();  
-  }, 2000);  
+  }, 5000);  
 });
