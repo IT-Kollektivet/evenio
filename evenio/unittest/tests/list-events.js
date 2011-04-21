69e6a338e2a0a631a39ecd772dccc0fd5d66b54a
dotnetCarpenter <jon.ronnenberg@gmail.com>
2011-04-21 23:33:38 +0200

    moved unit test to public folder

---
+++ evenio/unittest/tests/list-events.js
@@ -0,0 +1,47 @@
+/**
+ * tutorial for async testing: http://net.tutsplus.com/tutorials/javascript-ajax/how-to-test-your-javascript-code-with-qunit/
+ */
+function successCallback(data, textStatus, jqXHR) {
+  var status = data.status;
+  ok(true, status);
+}
+function failedCallback(data, textStatus, jqXHR) {
+  var status = data.status;
+  ok(false, status);
+}
+
+module("List Events", {
+  setup: function () {
+    this.uriBase = "/evenio/";
+    //this.uriBase = "http://192.168.1.102:8000/evenio/";
+    $.ajaxSetup({
+      async: true
+    });
+  },
+  teardown: function () {
+
+  }
+});
+/* Test#1 */
+test('Testing event lists', 3, function () {
+  stop();
+
+  $.ajax(this.uriBase, {
+    success: successCallback,
+    error: failedCallback
+  });
+  
+  $.ajax(this.uriBase + '2011/05/', {
+    success: successCallback,
+    error: failedCallback
+  });
+
+  $.ajax(this.uriBase + '2011/05/01', {
+    success: successCallback,
+    error: failedCallback
+  });
+  
+  setTimeout(function() {  
+    start();  
+  }, 5000);  
+});
