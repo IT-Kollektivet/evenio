15dc5f4ea73b1eca25a0fdc31545f942277b0d5c
Benjamin Bach <benjamin@botch.(none)>
2011-07-16 22:46:22 +0200

    Some more styling and changes to login view

--- testproject/evenio_site/urls.py
+++ testproject/evenio_site/urls.py
@@ -4,4 +4,5 @@ from django.http import Http404, HttpResponse
 urlpatterns = patterns('',
     url(r'^$', 'evenio_site.views.frontpage', name='frontpage'),
     url(r'^login-redirect/$', 'evenio_site.views.login_and_redirect', name='login_and_redirect'),
+    url(r'^logout/$', 'evenio_site.views.logout', name='logout'),
 )
--- testproject/evenio_site/views.py
+++ testproject/evenio_site/views.py
@@ -1,31 +1,22 @@
 # Create your views here.
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from django.contrib.auth.views import auth_login
-from django.contrib.auth import login
+from django.contrib.auth.views import login, logout as auth_logout
+from django.utils.translation import ugettext_lazy as _
 
 import forms
 
 def login_and_redirect(request):
-    if request.method == 'POST':
-        form = forms.EvenioAuthenticationForm(request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(user)
-            redirect_to = form.cleaned_data["redirect_to"]
-            if redirect_to:
-                return redirect(redirect_to)
-            else:
-                return redirect('frontpage')
-        else:
-            print form.errors
-    else:
-        form = forms.EvenioAuthenticationForm()
-    context = RequestContext(request, {'login_form': form})
-    return render_to_response("login.html", context)
+    return login(request, template_name='login.html',
+                 redirect_field_name='next',
+                 authentication_form=forms.EvenioAuthenticationForm,
+                 extra_context={'title': _(u"Log ind")})
+
+def logout(request):
+    return auth_logout(request, next_page='/')
 
 def frontpage(request):
     
-    context = RequestContext(request)
+    context = RequestContext(request, {'title': _(u"Frontpage")})
     return render_to_response("frontpage.html", context)
     
