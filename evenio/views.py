20f4ee7ddbc423b6625f995a9853dde3bf9113c4
Simon Shine <shine@diku.dk>
2011-07-16 18:46:10 +0200

    Ændret templates til dansk

--- evenio/views.py
+++ evenio/views.py
@@ -4,6 +4,7 @@ Views for evenio
 from django.views.generic import UpdateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import CreateView
 
 from django.core.serializers import serialize
 from django.http import HttpResponse, Http404
